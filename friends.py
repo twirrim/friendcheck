f932fade36f3cec01a85c05c87a7aa688f537d00
twirrim <izAe3BDA?>
2012-02-11 18:53:35 +0000

    Tidied up code, added comments, moved database definition to config file

--- friends.py
+++ friends.py
@@ -4,32 +4,44 @@ import cPickle
 import sqlite3
 import os.path
 import sys
+import ConfigParser
 
-conkey=''
-consec=''
-accesstok=''
-accesstoksec=''
+config = ConfigParser.RawConfigParser()
+config.read("friends.cfg")
 
-if not os.path.exists('./friends.sqlite3'):
-    conn = sqlite3.connect('./friends.sqlite3')
+conkey = config.get("twitter","consumer_key")
+consec = config.get("twitter","consumer_secret")
+accesstok = config.get("twitter","access_token")
+accesstoksec = config.get("twitter","access_token_secret")
+database = config.get("friends","database")
+
+# Check if the database exists or not, create if necessary
+if not os.path.exists(database):
+    conn = sqlite3.connect(database)
     c = conn.cursor()
     c.execute('''create table friends
               (friend text)''')
     conn.commit()
     c.close()
     
+# Try and establish a connection with the database
 try:
-    conn = sqlite3.connect('./friends.sqlite3')
+    conn = sqlite3.connect(database)
     c = conn.cursor()
 except:
-    print "Failed to connect to sqlite3"
+    print "Failed to connect to sqlite3 database"
 
+# Try to connect to the Twitter API
 try:
     api = twitter.Api(consumer_key=conkey,consumer_secret=consec, access_token_key=accesstok, access_token_secret=accesstoksec)
 except:
     print "Failed to connect to the API"
+
+# Verifying all credentials are good
 api.VerifyCredentials()
 
+# Get a dictionary object containing all the IDs of your friends
+# If userid is new, adds to database
 f = api.GetFriendIDs()
 for fid in  f['ids']:
     t = (fid,)     
@@ -37,8 +49,11 @@ for fid in  f['ids']:
     friend = c.fetchone()
     if not friend:
         c.execute('insert into friends values (?)',t)
-
+# Commits the changes
 conn.commit()
+
+# Grab a list of people user used to follow
+# Report on changes.
 
 c.execute('select * from friends')
 for of in c:
